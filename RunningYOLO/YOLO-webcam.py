310cae0d4fa50dc23e14498208b6272959061693
Piotr Kacprzak <piotrkac4@gmail.com>
2024-08-05 21:16:37 +0200

    Confidence level of prediction added to live webcam predictions

--- RunningYOLO/YOLO-webcam.py
+++ RunningYOLO/YOLO-webcam.py
@@ -1,5 +1,6 @@
 from ultralytics import YOLO
 import cv2
+import cvzone
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 1280)
@@ -16,6 +17,10 @@ while True:
             x1, y1, x2, y2 = box.xyxy[0]
             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
             cv2.rectangle(img, (x1, y1), (x2, y2), (0, 200, 0), 3)
+
+            conf = round(float(box.conf[0]), 2)
+
+            cvzone.putTextRect(img=img, text=f"{conf}", pos=(max(0, x1), max(35, y1-20)))
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
