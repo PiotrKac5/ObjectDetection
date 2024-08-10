8d89473789d4a7e922270d59a89476f3e4dac40f
Piotr Kacprzak <piotrkac4@gmail.com>
2024-08-10 19:23:05 +0200

    YOLOv10 model tried

--- RunningYOLO/YOLO-webcam.py
+++ RunningYOLO/YOLO-webcam.py
@@ -6,7 +6,8 @@ cap = cv2.VideoCapture(0)
 cap.set(3, 1280)
 cap.set(4, 720)
 
-model = YOLO("../Yolo-Weights/yolov8n.pt")
+# model = YOLO("../Yolo-Weights/yolov8n.pt")
+model = YOLO("../Yolo-Weights/yolov10n.pt")
 
 classNames = [
             "person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
